fcd1e7db566755bde87c187c5c27f6ea819ebcf2
phetzy <david.j.fetzer@gmail.com>
2023-05-04 15:03:28 -0400

    retrieve and ingest data to elastic

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const client = require('./elasticsearch/client');
+
 const app = express();
+
 const port = 3001;
+
+const data = require('./data_management/retrieve_and_ingest_data');
+
+app.use('/ingest_data', data); 
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
